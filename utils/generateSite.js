6303542a1a8de3accbb819d2a64b9deb7c55b6d9
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-20 07:25:27 -0500

    finished writing generating code, new err(function doesnt get invoked in terminal)

--- utils/generateSite.js
+++ utils/generateSite.js
@@ -72,26 +72,26 @@ module.exports= employeeSections => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie-edge">
       <title>MyTeam</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
     </head>
   
     <body>
     <header>
-      <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Constructor</h1>
-      </div>
+      Team Profile
     </header>
 
-    <main class="container my-5">
+    <div class="container">
           
       ${genereateTeam(employeeSections)}
 
-    </main>
+    </div>
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; </h3>
     </footer>
+    <script src="../utils/generateSite.js"></script>
+    <script src="../dist/script.js"></script>
   </body>
   </html>
   `;
