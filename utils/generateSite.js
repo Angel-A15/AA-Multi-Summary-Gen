4a97cb64030fead27d41559ab255ce54f83d7c20
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-21 19:07:13 -0500

    added text center and padding

--- utils/generateSite.js
+++ utils/generateSite.js
@@ -82,12 +82,12 @@ module.exports= employeeSections => {
     <header>
     <div class="container flex-row justify-space-between align-center py-3">
       
-      <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
+      <h1 class="page-title text-white text-center bg-dark py-2 px-3">My Team</h1>
 
     </div>
     </header>
 
-    <div class="container">
+    <div class="container px-3 pt-3">
           
       ${genereateTeam(employeeSections)}
 
